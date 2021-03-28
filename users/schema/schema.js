2547e2ccb14edfbe27e3989dd7f8afb9f38e4540
Brady Peterson <bpeterson1663@gmail.com>
2021-03-27 22:45:09 -0500

    feat(relation): create circular relation between users and companies

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -5,21 +5,29 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        user: {
+            type: new GraphQLList(UserType),
+            resolve: (parentValue, args) => {
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(resp => resp.data)
+            }
+        }
+    })
 })
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
@@ -30,7 +38,7 @@ const UserType = new GraphQLObjectType({
                     .then(res => res.data)
             }
         }
-    }
+    })
 })
 // root query - the entry point into our data
 const RootQuery = new GraphQLObjectType({
@@ -42,6 +50,14 @@ const RootQuery = new GraphQLObjectType({
             resolve: (parentValue, args) => {
                 //reaches out and grabs
                 return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data)
+            }
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString }},
+            resolve: (parentValue, args) => {
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
                     .then(resp => resp.data)
             }
         }
