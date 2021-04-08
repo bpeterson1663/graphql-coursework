83166d58239ae3f259a1e6c1a6f750fe8ddf8176
Brady Peterson <bpeterson1663@gmail.com>
2021-04-07 22:08:19 -0500

    feat(song form): implement song form

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useQuery, gql } from '@apollo/client'
+import SongForm from './SongForm'
 const query = gql`
    { 
        songs{
@@ -11,10 +12,13 @@ const query = gql`
 
  const SongList = () => {
     const { loading, error, data } = useQuery(query)
-    console.log(loading, error, data)
     if(loading) return 'Loading....'
+    const createNewSong = (songTitle) => {
+        console.log("song title ", songTitle)
+    }
     return (
         <div>
+            <SongForm handleOnSubmit={createNewSong} />
             Song List
             {data.songs.map(song => <div key={song.id}>{song.title}</div>)}
         </div>
