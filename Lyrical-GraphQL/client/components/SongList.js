55724895c060dcb59153dc7404a359561b19f511
Brady Peterson <bpeterson1663@gmail.com>
2021-04-08 22:47:59 -0500

    feat(delete song): add delete song mutation

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
 import SongForm from './SongForm'
 import { fetchSongs } from '../queries/fetchSongs'
 
+const deleteMutation = gql`
+    mutation deleteSong($id: ID){
+        deleteSong(id: $id){
+            id
+        }
+    }
+`
 
  const SongList = () => {
     const { loading, error, data } = useQuery(fetchSongs)
+    const [deleteSongTitle, {loading: deleteLoading}] = useMutation(deleteMutation)
     if(loading) return 'Loading....'
+    if(deleteLoading) console.log("DELETEING")
+    const handleDelete = (id) => {
+        deleteSongTitle({
+            variables: { id: id},
+            refetchQueries: [{query: fetchSongs}]
+        })
+    }
     return (
         <div>
             <SongForm />
-            {data?.songs.map(song => <div key={song.id}>{song.title}</div>)}
+            {data?.songs.map(song => <div key={song.id}>{song.title}<button onClick={() => handleDelete(song.id)}>Delete</button></div>)}
         </div>
     )
 }
